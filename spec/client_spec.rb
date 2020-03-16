c87db8ce233cf5d74459495749b43951c671ecaa
Grey Moore <grey@controlshiftlabs.com>
2020-03-16 16:05:39 -0400

    non-Z spelling

--- spec/client_spec.rb
+++ spec/client_spec.rb
@@ -1,7 +1,7 @@
 require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
 describe TokyoApi::Client do
-  describe 'initialization' do
+  describe 'initialisation' do
     subject { described_class.new(options) }
 
     context 'with a basic_auth string parameter' do
